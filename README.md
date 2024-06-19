013cb49ed0ed3b459b030e141bd714df2e93ad11
Eva Emmerich <eva@evaemmerich.com>
2024-06-19 11:17:59 -0500

    lift string_to_macro to its own repo

---
+++ README.md
@@ -0,0 +1,2 @@
+simple script that takes a string and turns it into a zmk macro to add to a keymap
+this allows a single keypress to output precanned messages
---
+++ string_to_macro.py
@@ -0,0 +1,89 @@
+#!/bin/python3.10
+
+
+
+
+
+
+
+
+
+test_macro_string = """This is a really cool
+test1
+String
+"""
+
+
+
+char_to_kc = {
+    " "  : "SPC",
+    "\n": "ENTER",
+    "(" : "LEFT_PARENTHESIS",
+    ")" : "RIGHT_PARENTHESIS",
+    "[" : "LEFT_BRACKET",
+    "]" : "RIGHT_BRACKET",
+    "{" : "LEFT_BRACE",
+    "}" : "RIGHT_BRACE",
+    ":" : "COLON",
+    ";" : "SEMICOLON",
+    "'" : "SINGLE_QUOTE",
+    '"' : "DOUBLE_QUOTE",
+    "<" : "LESS_THAN",
+    ">" : "GRATER_THAN",
+    "/" : "SLASH",
+    "\\" : "BACKSLASH",
+    "|" : "PIPE",
+    "*" : "ASTERISK",
+    "%" : "PERCENT",
+    "&" : "AMPERSAND",
+    "$" : "DOLLAR",
+    "#" : "HASH",
+    "@" : "AT_SIGN",
+    "!" : "EXCLAMATION",
+    "." : "DOT",
+    "," : "COMMA",
+    "-" : "HYPEN",
+    "’" : "SINGLE_QUOTE",
+}
+
+def convert(name, input_string):
+
+    print(f"        {name}: {name} {{")
+    print('            compatible = "zmk,behavior-macro";')
+    print("            #binding-cells = <0>;")
+    print("            bindings")
+
+    first_entry = "                ="
+    same_line_entry = ","
+    new_line_entry = "                ,"
+
+    line_header = first_entry
+
+    for char in input_string:
+        if char.isupper():
+            print(f"{line_header} <&macro_press &kp LSHFT>", end="")
+            # everything after the first line uses the "same_line_entry"
+            line_header = same_line_entry
+            print(f"{line_header} <&macro_tap &kp {char}>", end="")
+            print(f"{line_header} <&macro_release &kp LSHFT>", end="")
+
+        elif char.isalpha():
+            print(f"{line_header} <&macro_tap &kp {char.upper()}>", end="")
+
+        elif char in char_to_kc.keys():
+            print(f"{line_header} <&macro_tap &kp {char_to_kc[char]}>", end="")
+
+        elif char.isnumeric():
+            print(f"{line_header} <&macro_tap &kp N{char}>", end="")
+        else:
+            raise RuntimeError(f"TODO: not implemented. cannot handle:{char}")
+
+        # everything after the first line uses the "same_line_entry"
+        line_header = same_line_entry
+
+    print("")
+    print("                ;")
+    print("        };")
+
+convert("test_string", test_macro_string)
+print("")
